157466d7e307a418754b0777861049359c7c8d8d
Setareh <sti.ri433@gmail.com>
2025-12-06 09:12:14 +0330

    Add files via upload

---
+++ Program(6).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication143
+{
+    class Program
+    {
+        static int Fib(int n)
+        {
+            if (n <= 2) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+              Console.WriteLine("add: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("fib");
+
+        for (int i = 1; i <= n; i++)
+            Console.Write(Fib(i) + " ");
+        Console.ReadKey();
+    
+        }
+    }
+}
